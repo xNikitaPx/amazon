de9b847c8de7ac28770d19edae246a7ba227725e
Nikita <b.boynikone@gmail.com>
2025-12-08 23:02:05 +0200

    Add cart

---
+++ data/cart.js
@@ -0,0 +1,4 @@
+"use strict";
+const cart = [];
+
+export { cart };
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { cart } from "../data/cart.js";
+
 const productGrid = document.querySelector(".js-products-grid");
 
 productGrid.innerHTML = products
@@ -31,7 +33,7 @@ productGrid.innerHTML = products
           )}</div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -47,12 +49,56 @@ productGrid.innerHTML = products
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart${product.id}">
             <img src="images/icons/checkmark.png" />
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">Add to Cart</button>
+          <button class="add-to-cart-button button-primary js-add-to-card" data-product-id="${
+            product.id
+          }">Add to Cart</button>
         </div>`;
   })
   .join("");
+
+const addBtns = document.querySelectorAll(".js-add-to-card");
+const cartQuantity = document.querySelector(".js-cart-quantity");
+
+addBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const productId = btn.dataset.productId;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+
+    const quantity = +quantitySelector.value;
+
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      cart.push({
+        productId,
+        quantity,
+      });
+    }
+
+    const addMessage = document.querySelector(`.js-added-to-cart${productId}`);
+
+    addMessage.classList.add("added-to-cart-visible");
+    setTimeout(() => {
+      addMessage.classList.remove("added-to-cart-visible");
+    }, 2000);
+
+    cartQuantity.innerText = cart.reduce((sum, item) => {
+      return (sum += item.quantity);
+    }, 0);
+  });
+});
